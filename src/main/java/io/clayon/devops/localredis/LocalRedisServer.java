e3fce56e3b73069431413185b970bae5376fef78
drs <drs@218.38.137.27>
2020-02-20 17:19:30 +0900

    init

---
+++ src/main/java/io/clayon/devops/localredis/LocalRedisServer.java
@@ -0,0 +1,32 @@
+package io.clayon.devops.localredis;
+
+import redis.embedded.RedisServer;
+
+public class LocalRedisServer {
+
+	public static void main(String[] args) throws Exception {
+		
+		int port = 16379;
+		
+		final RedisServer redisServer = new RedisServer(port);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				redisServer.stop();
+			}
+		});
+		
+		try {
+			redisServer.start();
+			System.out.println("Local Redis startup success port : " + port);
+		} catch (Exception e) {
+			System.out.println("Local Redis startup error");
+			
+			throw e;
+		} finally {
+			
+		}
+	}
+
+	
+}
